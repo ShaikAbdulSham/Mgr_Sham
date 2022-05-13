2d5a191cead18a614712f87c66e365138b914671
ShaikAbdulSham <abdulshameerashaik@gmail.com>
2022-05-13 21:03:14 +0530

    Junit latest modification

--- wdio.conf.js
+++ wdio.conf.js
@@ -135,7 +135,7 @@ exports.config = {
     reporters: ['spec', ['junit', {
         outputDir: './JunitReports',
         outputFileFormat: function(options) { // optional
-            return `results${new Date().toLocaleTimeString()}.xml`
+            return `results${new Date().getTime()}.xml`
         }
     }]],
 
